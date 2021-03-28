c5056168d04e02555256bef9b938e70effacd5cc
dhanu <andreasdhanu@gmail.com>
2021-03-28 12:47:38 +0700

    add

--- 1/1.1/src/main.go
+++ 1/1.1/src/main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-  curTime :=  time.Now().String()
+  random := md5.Sum([]byte(time.Now().String()))
   for {
-    fmt.Printf("%s: %x\n", time.Now().Format(time.RFC3339), md5.Sum([]byte(curTime)))
+    fmt.Printf("%s: %x\n", time.Now().Format(time.RFC3339), random)
     time.Sleep(5 * time.Second)
   }
 }
---
+++ 1/1.7/src/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "crypto/md5"
+  "fmt"
+  "net/http"
+  "log"
+  "time"
+)
+
+var (
+  random string
+)
+
+func main() {
+  random = fmt.Sprintf("%x", md5.Sum([]byte(time.Now().String())))
+
+  http.HandleFunc("/ping", pingHandler)
+  http.HandleFunc("/random", randomHandler)
+
+  log.Println("Starting server at port 8080")
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "GET" {
+    http.Error(w, "method is not supported", http.StatusNotFound)
+    return
+  }
+  fmt.Fprintf(w, "pong")
+}
+
+func randomHandler(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "GET" {
+    http.Error(w, "method is not supported", http.StatusNotFound)
+    return
+  }
+  fmt.Fprintf(w,"%s: %s\n", time.Now().Format(time.RFC3339), random)
+}
