d6e082c2df0021f34dcd471fc56162e9d604f9aa
dhanu <andreasdhanu@gmail.com>
2021-03-30 08:13:02 +0700

    add

---
+++ 1/1.11/app/src/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"log"
+  "io/ioutil"
+	"net/http"
+	"time"
+)
+
+var (
+	random  string
+)
+
+func main() {
+	random = fmt.Sprintf("%x", md5.Sum([]byte(time.Now().String())))
+
+	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/random", randomHandler)
+
+	log.Println("Starting server at port 8080")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "pong")
+}
+
+func randomHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+  b, err := ioutil.ReadFile("./shared/test")
+  if err != nil {
+    log.Println(err)
+    b = []byte("0")
+  }
+  fmt.Fprintf(w, "%s: %s\nPing/ Pongs: %s", time.Now().Format(time.RFC3339), random, string(b))
+}
+
---
+++ 1/1.11/pingpong/src/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"net/http"
+	"strconv"
+	"path/filepath"
+)
+
+var (
+	counter int
+)
+
+func main() {
+	http.HandleFunc("/pingpong", pingpongHandler)
+
+	log.Println("Starting server at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func pingpongHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "pong %d", counter)
+	counter++
+	writeToVolume("./shared/test", counter)
+}
+
+func writeToVolume(p string, c int) {
+	if err := os.MkdirAll(filepath.Dir(p), 0770); err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.Create(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	_, err = f.WriteString(strconv.Itoa(c))
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Sync()
+}
