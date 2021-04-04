b6d2b445ab6eedeb94fb1f06d51b965a090fa1e8
dhanu <andreasdhanu@gmail.com>
2021-04-04 23:46:56 +0700

    fix

--- 1/1.12/src/main.go
+++ 1/1.12/src/main.go
@@ -1,13 +1,54 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+var (
+	imageData string
+)
+
 func main() {
-  http.Handle("/image", http.FileServer(http.Dir("https://picsum.photos/1200")))
+	http.HandleFunc("/image", imageHandler)
 	log.Println("Starting server at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusMethodNotAllowed)
+		return
+	}
+
+  data, err := getImage("https://picsum.photos/1200")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed when get image, err: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+  imageData = data
+
+	fmt.Fprint(w, imageData)
+}
+
+func getImage(url string) (string, error) {
+	if len(imageData) > 0 {
+		return imageData, nil
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	respData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(respData), nil
+}
