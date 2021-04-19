2fc805ab201bd41c24c84c659c9d1470d4a4d4a0
dhanu <andreasdhanu@gmail.com>
2021-04-19 08:48:17 +0700

    add

--- 2/2.2/frontend/src/main.go
+++ 2/2.2/frontend/src/main.go
@@ -1,26 +1,52 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
+
+var (
+	client = &http.Client{Timeout: time.Second * 10}
+)
+
+// GraphqlResponse ...
+type GraphqlResponse struct {
+	Data interface{} `json:"data"`
+}
+
+// Todos ...
+type Todos struct {
+	Todos []Todo `json:"todos"`
+}
+
+// Todo ...
+type Todo struct {
+	ID   string `json:"id"`
+	Text string `json:"text"`
+}
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.Handle("/assets/", http.FileServer(http.Dir("./shared")))
+	http.HandleFunc("/submit", submitHandler)
 
-	log.Println("Starting server at port 8081")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./shared"))))
+
+	log.Println("Starting server at port 3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
-		http.Error(w, "method is not supported", http.StatusMethodNotAllowed)
+		http.Error(w, "method is not supported", http.StatusBadRequest)
 		return
 	}
 
@@ -35,11 +61,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprint(w, "<html><body>")
-	fmt.Fprint(w, "<img src='assets/test' alt='test' style='width:300px;height:300px;'></br></br>")
-	fmt.Fprint(w, "<input type='text' id='todo' name='todo'> <button type='button'>Create TODO</button></br>")
-	fmt.Fprint(w, "<ul><li>TODO 1</li><li>TODO 2</li></ul>")
-	fmt.Fprint(w, "</html></body>")
+	query := fmt.Sprintf("{\"query\":\"query listTodo {\\n    todos {\\n      id\\n      text\\n    }\\n}\",\"variables\":{}}")
+
+	respBody, statusCode, err := reqBackend(query)
+	if err != nil {
+		http.Error(w, err.Error(), statusCode)
+		return
+	}
+
+	graphqlResp := &GraphqlResponse{Data: &Todos{}}
+
+	if err := json.Unmarshal([]byte(respBody), graphqlResp); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	t, _ := template.ParseFiles("view.html")
+	if err := t.Execute(w, graphqlResp); err != nil {
+		http.Error(w, fmt.Sprintf("failed when execute template, err: %v", err), http.StatusInternalServerError)
+		return
+	}
 }
 
 func downloadImageToVolume(url string, path string) error {
@@ -80,4 +121,56 @@ func readImageFromVolume(path string) (string, error) {
 	}
 
 	return string(b), nil
+}
+
+func submitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "method is not supported", http.StatusBadRequest)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	todo := r.FormValue("todo")
+
+	query := fmt.Sprintf("{\"query\":\"mutation createTodo ($todo:String!) {\\n  createTodo(input:{text:$todo}) {\\n    text\\n  }\\n}\",\"variables\":{\"todo\":\"%s\"}}", todo)
+
+	_, statusCode, err := reqBackend(query)
+	if err != nil {
+		http.Error(w, err.Error(), statusCode)
+		return
+	}
+}
+
+func reqBackend(query string) (respBody string, statusCode int, err error) {
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:8080/query", bytes.NewBuffer([]byte(query)))
+	if err != nil {
+		return "", http.StatusInternalServerError, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", http.StatusInternalServerError, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", http.StatusInternalServerError, err
+	}
+
+	if !isStatusCode2xx(resp.StatusCode) {
+		return "", resp.StatusCode, fmt.Errorf("failed with statusCode: %d, body: %s", resp.StatusCode, string(body))
+	}
+
+	return string(body), resp.StatusCode, nil
+}
+
+func isStatusCode2xx(statusCode int) bool {
+	return statusCode >= 200 && statusCode < 300
 }
