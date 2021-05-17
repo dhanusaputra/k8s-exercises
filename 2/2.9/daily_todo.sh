ed13f1e31c8af656c92eda9ddb5d69b8583117de
dhanu <andreasdhanu@gmail.com>
2021-05-17 12:41:01 +0700

    add

--- 2/2.9/daily_todo.sh
+++ 2/2.9/daily_todo.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+random=$(curl -sLI https://en.wikipedia.org/wiki/Special:Random | grep "^location:" | cut -d " " -f2 | tr -d "\r")
+
 if [ "$URL" ]
 then
-curl -L -X POST "$URL" \
+  curl -sL -X POST "$URL" \
 -H 'Content-Type: application/json' \
---data-raw '{"query":"mutation createTodo ($todo:String!,$done:Boolean!) {\n  createTodo(input:{text:$todo,done:$done}) {\n    text\n    done\n  }\n}","variables":{"todo":"test todo2","done":false}}'
+--data-raw '{"query":"mutation createTodo ($todo:String!,$done:Boolean!) {\n  createTodo(input:{text:$todo,done:$done}) {\n    text\n    done\n  }\n}","variables":{"todo":"Read '"$random"'","done":false}}'
 fi
